1a10f6e73f496d8d7751868504b97ca64f134323
Carina Reketis <carina.reketis@gmail.com>
2019-07-21 11:18:03 -0300

    inserting bootstrap and page layout

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const path = require('path');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const open = require('gulp-open');
+
+const Paths = {
+  HERE: './',
+  DIST: 'dist/',
+  CSS: './assets/css/',
+  SCSS_TOOLKIT_SOURCES: './assets/scss/blk-design-system.scss',
+  SCSS: './assets/scss/**/**'
+};
+
+gulp.task('compile-scss', function() {
+  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write(Paths.HERE))
+    .pipe(gulp.dest(Paths.CSS));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(Paths.SCSS, ['compile-scss']);
+});
+
+gulp.task('open', function() {
+  gulp.src('index.html')
+    .pipe(open());
+});
+
+gulp.task('open-app', ['open', 'watch']);
